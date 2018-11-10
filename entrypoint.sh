3f539fd0a473c43bd6f0c26f3a5ca8a7fc468c15
Wizos <Wizos@qq.com>
2018-11-11 01:49:09 +0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ wget -O goflyway.tar.gz https://github.com/coyove/goflyway/releases/download/2.0
 tar -zxf goflyway.tar.gz
 #cd /goflyway/goflyway_linux_amd64
 chmod +x goflyway 
- ./goflyway -k=30522b10-a8d2-11e8-978c-f0d5bf44df99  -l=":$PORT"
+ ./goflyway -k="30522b10-a8d2-11e8-978c-f0d5bf44df99"  -l=":233"
